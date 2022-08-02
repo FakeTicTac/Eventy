a2f37dda21747219cfa4d67e20081304b11a1577
unknown <romake@ttu.ee>
2022-08-02 14:01:00 +0300

    Database Design Implementation.

---
+++ Eventy/App.Domain/Event.cs
@@ -0,0 +1,58 @@
+﻿using Base.Domain;
+using App.Domain.Identity;
+using Base.Contracts.Domain;
+using Base.Domain.Translation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+// ReSharper disable CollectionNeverUpdated.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable PropertyCanBeMadeInitOnly.Global
+
+
+namespace App.Domain;
+
+
+/// <summary>
+/// Application Event Table Implementation.
+///  - Defines Specific Entity Rows for Event. 
+/// </summary>
+// ReSharper disable once ClassNeverInstantiated.Global
+public class Event : DomainEntityUser<AppUser>, IDomainEntityId
+{
+
+    /// <summary>
+    /// Defines Event Title.
+    /// </summary>
+    [Column(TypeName = "jsonb")]
+    public LanguageString Title { get; set; } = new();
+    
+    
+    /// <summary>
+    /// Defines Event Description.
+    /// </summary>
+    [Column(TypeName = "jsonb")]
+    public LanguageString Description { get; set; } = new();
+
+    
+    /// <summary>
+    /// Defines Event Full Address and Location.
+    /// </summary>
+    [MaxLength(256)]
+    public string? Address { get; set; }
+    
+    
+    /// <summary>
+    /// Defines Event Starting Time.
+    /// </summary>
+    public DateTime? StartTime { get; set; }
+    
+    
+    /// <summary>
+    /// Defines Event Cover Image Path on Server Side.
+    /// </summary>
+    [MaxLength(260)]
+    public string? CoverImagePath { get; set; }
+    
+}
---
+++ Eventy/App.Domain/Identity/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Base.Domain.Identity;
+
+
+// ReSharper disable CollectionNeverUpdated.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable PropertyCanBeMadeInitOnly.Global
+
+
+namespace App.Domain.Identity;
+
+
+/// <summary>
+/// Application Identity User Implementation.
+///  - Defines Specific Entity Rows for Identity User. 
+/// </summary>
+// ReSharper disable once ClassNeverInstantiated.Global
+public class AppUser : BaseUser { }
